c2e71ff31f65d525375238d0721c36694f5bc5bc
anubis <anubis@icpleague.com>
2022-06-27 16:55:22 +0800

    187

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,105 +1,79 @@
 struct Solution;
 
 // impl Solution {
-//     pub fn largest_number(nums: Vec<i32>) -> String {
-//         if !nums.iter().any(|n| *n != 0) {
-//             return "0".to_string();
+//     pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+//         use std::collections::HashSet;
+
+//         let s = s.as_bytes();
+
+//         let mut set: HashSet<&[u8]> = HashSet::new();
+//         // println!("{:?}", set);
+//         let mut rs: HashSet<&[u8]> = HashSet::new();
+
+//         let mut i = 0;
+//         while i + 10 <= s.len() {
+//             let ss = &s[i..i+10];
+//             // println!("{:?} {}", set, i);
+//             // println!("{:?}",  set.contains(ss));
+//             if set.contains(ss) {
+//                 rs.insert(ss);
+//                 // println!("{:?}", ss);
+//             } else {
+//                 set.insert(ss);
+//             }
+//             i += 1;
 //         }
 
-//         let mut nums = nums;
-//         nums.sort_by(|a, b| {
-//             let a = format!("{}{}", a, b);
-//             let b = format!("{}{}", b, a);
-//             b.cmp(&a)
-//         });
-//         let mut res = String::new();
-//         for num in nums {
-//             res.push_str(&num.to_string());
-//         }
-//         res
+//         use std::borrow::Cow;
+
+//         rs.into_iter()
+//         .map(|a| String::from_utf8_lossy(a))
+//         .map(|a| match a {
+//             Cow::Borrowed(v) => v.to_string(),
+//             Cow::Owned(v) => v,
+//         })
+//         .collect::<Vec<String>>()
 //     }
 // }
 
 impl Solution {
-    pub fn largest_number(nums: Vec<i32>) -> String {
-        if !nums.iter().any(|n| *n != 0) {
-            return "0".to_string();
+    pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+        use std::collections::HashMap;
+
+        let s = s.as_bytes();
+
+        let mut map: HashMap<&[u8], usize> = HashMap::new();
+
+        let mut i = 0;
+        while i + 10 <= s.len() {
+            let ss = &s[i..i + 10];
+            // println!("{:?} {}", set, i);
+            // println!("{:?}",  set.contains(ss));
+            let v = map.entry(ss).or_insert(0);
+            *v += 1;
+            i += 1;
         }
 
-        let mut nums = nums.iter().map(|n| n.to_string()).collect::<Vec<String>>();
-        nums.sort_by(|a, b| {
-            let a = format!("{}{}", a, b);
-            let b = format!("{}{}", b, a);
-            b.cmp(&a)
-        });
-        let mut res = String::new();
-        for num in nums {
-            res.push_str(&num.to_string());
-        }
-        res
+        use std::borrow::Cow;
+
+        map.into_iter()
+            .filter(|a| a.1 > 1)
+            .map(|a| String::from_utf8_lossy(a.0))
+            .map(|a| match a {
+                Cow::Borrowed(v) => v.to_string(),
+                Cow::Owned(v) => v,
+            })
+            .collect::<Vec<String>>()
     }
 }
 
-// impl Solution {
-//     pub fn largest_number(nums: Vec<i32>) -> String {
-//         if !nums.iter().any(|n| *n != 0) {
-//             return "0".to_string();
-//         }
-
-//         use std::cmp::Ordering;
-
-//         fn compare(a: &Vec<u8>, b: &Vec<u8>, i: usize) -> Ordering {
-//             if a.len() <= i {
-//                 if b.len() <= i {
-//                     return Ordering::Equal;
-//                 } else {
-//                     return Ordering::Less;
-//                 }
-//             } else {
-//                 if b.len() <= i {
-//                     return Ordering::Greater;
-//                 } else {
-//                     let a0 = a[i];
-//                     let b0 = b[i];
-//                     match a0.cmp(&b0) {
-//                         Ordering::Greater => Ordering::Less,
-//                         Ordering::Less => Ordering::Greater,
-//                         Ordering::Equal => {
-//                             if a.len() == b.len() {
-//                                 return compare(a, b, i + 1);
-//                             } else {
-//                                 let mut aa = a.clone();
-//                                 let mut aa2 = a.clone();
-//                                 let mut bb = b.clone();
-//                                 let mut bb2 = b.clone();
-//                                 aa.append(&mut bb2);
-//                                 bb.append(&mut aa2);
-//                                 return compare(&aa, &bb, 0);
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-
-//         let mut nums: Vec<Vec<u8>> = nums.iter().map(|n| n.to_string().into_bytes()).collect();
-//         nums.sort_by(|a, b| compare(a, b, 0));
-//         let mut res = String::new();
-//         for num in nums {
-//             for n in num {
-//                 res.push(char::from(n));
-//             }
-//         }
-//         res
-//     }
-// }
-
 fn main() {
-    println!("{}", 179);
+    println!("{}", 187);
 
     // let hp = Solution::largest_number([1, 2, 9, 9, 91].to_vec());
     // let hp = Solution::largest_number([1, 2].to_vec());
-    let hp = Solution::largest_number([3, 30, 34, 5, 9].to_vec());
+    // let r = Solution::find_repeated_dna_sequences(String::from("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+    let r = Solution::find_repeated_dna_sequences(String::from("AAAAAAAAAAAAA"));
 
-    println!("{}", hp);
+    println!("{:#?}", r);
 }
